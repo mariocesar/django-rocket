525234a380a399743e3baafbdf26aa2bd65deb58
Mario César Señoranis Ayala <mariocesar@creat1va.com>
2014-06-05 09:19:02 -0400

    Skip test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,3 +1,4 @@
+from unittest import skip
 from django.test.utils import override_settings
 from django.test import TestCase
 
@@ -6,6 +7,7 @@ from django.test import TestCase
 class DjangoRocketIntegrationTestCase(TestCase):
     urls = 'tests.urls'
 
+    @skip('maybe moving integration tests to the example project')
     def test_anonymous_visit(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
